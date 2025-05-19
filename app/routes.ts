b3c9a3ba486a9e362ab33d769fe8d1150476c0ba
Mehzia <mehziashaik@gmail.com>
2025-05-19 16:36:51 -0400

    Using Queries

--- app/routes.ts
+++ app/routes.ts
@@ -3,4 +3,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
     index("routes/home.tsx"),
     route("/login", 'Formik/login.tsx'),
+    route("/query", 'Queries/index.tsx'),
 ] satisfies RouteConfig;
