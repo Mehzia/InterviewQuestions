6d0234c39613a18117c7f0c4c615a17ba91c2be9
Mehzia <mehziashaik@gmail.com>
2025-05-29 13:06:39 -0400

    overviewnav

--- app/patient/Overview/index.tsx
+++ app/patient/Overview/index.tsx
@@ -1,8 +1,12 @@
+import Nav from "./nav";
+
 function Overview() {
 return (
     <div>
     <h1 className="text-3xl mt-20 font-bold">Alena Bator</h1>
+    <Nav />
     </div>
+
 )
 }
 export default Overview;
